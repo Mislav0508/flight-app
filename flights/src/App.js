3ac2a417a490643948edd6f07de01f71351e4782
Mislav <mislav0508@hotmail.com>
2023-02-07 14:09:14 -0500

    initial push

---
+++ flights/src/App.js
@@ -0,0 +1,17 @@
+import Login from './Components/Login'
+import "./App.css"
+import { useSelector } from 'react-redux'
+import { selectUser } from './features/userSlice'
+import Logout from './Components/Logout'
+
+const App = () => {
+  const user = useSelector(selectUser)
+
+  return (
+    <div>
+      {user ? <Logout /> : <Login />}      
+    </div>
+  )
+}
+
+export default App
